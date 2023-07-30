86412ac412ed367aa080e83aed33d5f82e973a86
Tonmoy-Roy <roytonmoy901@gmail.com>
2023-07-30 11:17:16 +0600

    commit first

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './Header.css'
+import logo from '../../../images/Logo.svg'
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/order">Order </a>
+                <a href="/order-review">Review </a>
+                <a href="/inventory">Inventory    </a>
+                <a href="/Login">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
